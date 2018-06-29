f573d3147db4789e1edcc210d2d3beb1f68c6eed
dedyrosyadi <dedy.rosyadi@gmail.com>
2018-06-29 11:35:11 +0800

    styling form login

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -7,7 +7,7 @@
 	<title>Login</title>
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body style="    background: #27a859;">
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 			<div class="login-panel panel panel-default">
