8c443b7d0face59114dcb61c5447e300480c9f4a
dedyrosyadi <dedy.rosyadi@gmail.com>
2018-06-29 10:55:38 +0800

    styling css login form

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -11,7 +11,12 @@
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 			<div class="login-panel panel panel-default">
-				<div class="panel-heading">Log in</div>
+				<div class="text-center">
+					<img class="login-logo" src="{{asset('img/logo.png')}}">					
+				</div>
+
+
+				<div class="panel-heading">Silahkan Login !</div>
 				<div class="panel-body">
 					<form action="{{ route('login') }}" method="post">
 						{{ csrf_field() }}
@@ -22,10 +27,14 @@
 							<div class="form-group">
 								<input class="form-control" placeholder="Password" name="password" type="password">
 							</div>
-							<button type="submit" class="btn btn-primary" name="button">Login</button>
 						</fieldset>
 					</form>
 				</div>
+				<div class="panel-footer">
+					<a href="#" class="forget-pass pull-right">Lupas Password?</a>
+					<button type="submit" class="btn btn-primary btn-block" name="button">Login</button>
+				</div>
+
 			</div>
 		</div>
 	</div>
