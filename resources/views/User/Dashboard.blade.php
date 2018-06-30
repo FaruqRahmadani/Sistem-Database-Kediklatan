3889374388c7760da1dae762e31c38528abe9056
dedyrosyadi <dedy.rosyadi@gmail.com>
2018-06-30 17:13:49 +0800

    update dashboard

--- resources/views/User/Dashboard.blade.php
+++ resources/views/User/Dashboard.blade.php
@@ -7,12 +7,7 @@
 			<div class="panel-body">
 				<blockquote><h2 style="margin:0;">Selamat datang di Sistem Database Kediklatan</h2>BBPP Binuang - Kalimantan Selatan</blockquote>
 				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Sistem Database Kediklatan dapat membantu dalam pengelolaan data kediklatan seperti data pelatihan, data penyuluh, data komoditas, dan data peserta pelatihan.</p>
 				
 			</div>
 		</div>
