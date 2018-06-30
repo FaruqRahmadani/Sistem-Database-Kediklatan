ebed906df701d3d9f4dea8596949c7c54316543e
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-06-30 23:38:10 +0800

    add database kelompok tani

---
+++ database/migrations/2018_06_30_151054_create_kelompok_tanis_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKelompokTanisTable extends Migration
+{
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up()
+  {
+    Schema::create('kelompok_tanis', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('nama');
+      $table->string('nama_ketua');
+      $table->string('nomor_hp');
+      $table->string('alamat');
+      $table->integer('provinsi_id');
+      $table->integer('kota_id');
+      $table->integer('komoditas_id');
+      $table->integer('pelatihan_id');
+      $table->string('foto')->default('default.png');
+      $table->timestamps();
+    });
+  }
+
+  /**
+  * Reverse the migrations.
+  *
+  * @return void
+  */
+  public function down()
+  {
+    Schema::dropIfExists('kelompok_tanis');
+  }
+}
