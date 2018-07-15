c58f208575757c2c26be1fa052246c9354c58e0b
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-07-15 22:24:02 +0800

    soft delete komoditas

--- app/Komoditas.php
+++ app/Komoditas.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Komoditas extends Model
 {
+  use SoftDeletes;
+
   protected $fillable = [
     'nama',
     'keterangan',
