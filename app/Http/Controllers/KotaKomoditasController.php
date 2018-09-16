e262c039fda1b50e2c64e80f858f92397fd08bfd
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-09-16 09:54:20 +0800

    fungsi tambah data kota komoditas

--- app/Http/Controllers/KotaKomoditasController.php
+++ app/Http/Controllers/KotaKomoditasController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use Crypter;
-
 use App\KotaKomoditas;
 use App\KelompokTani;
 use App\Komoditas;
 use App\Kota;
+use HCrypt;
 
 class KotaKomoditasController extends Controller
 {
@@ -19,28 +17,28 @@ class KotaKomoditasController extends Controller
     return view('KotaKomoditas.Data', compact('Kota'));
   }
 
-  public function Tambah(){
+  public function TambahForm(){
     $Komoditas = Komoditas::all();
     return view('KotaKomoditas.Tambah', compact('Komoditas'));
   }
 
-  public function submitTambah(Request $request){
+  public function TambahSubmit(Request $request){
     $Kota = Kota::findOrFail($request->kota_id);
     $Kota->Komoditas()->sync($request->komoditas_id);
-    return redirect()->Route('Data-Kota-Komoditas')->with(['alert' => true, 'tipe' => 'success', 'judul' => 'Berhasil', 'pesan' => 'Tambah Data Berhasil']);
+    return redirect()->Route('kotaKomoditasData')->with(['alert' => true, 'tipe' => 'success', 'judul' => 'Berhasil', 'pesan' => 'Tambah Data Berhasil']);
   }
 
   public function Edit($Id){
-    $Id = Crypter::Decrypt($Id);
+    $Id = HCrypt::Decrypt($Id);
     $Kota = Kota::findOrFail($Id);
     $Komoditas = Komoditas::all();
     return view('KotaKomoditas.Edit', compact('Kota', 'Komoditas'));
   }
 
   public function submitEdit(Request $request, $Id){
-    $Id = Crypter::Decrypt($Id);
+    $Id = HCrypt::Decrypt($Id);
     $Kota = Kota::findOrFail($Id);
     $Kota->Komoditas()->sync($request->komoditas_id);
-    return redirect()->Route('Data-Kota-Komoditas')->with(['alert' => true, 'tipe' => 'success', 'judul' => 'Berhasil', 'pesan' => 'Edit Data Berhasil']);
+    return redirect()->Route('kotaKomoditasData')->with(['alert' => true, 'tipe' => 'success', 'judul' => 'Berhasil', 'pesan' => 'Edit Data Berhasil']);
   }
 }
