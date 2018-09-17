acfb10307b25972da578fd0f0145cdfe56705f9b
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-09-17 22:32:14 +0800

    refactor js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,9 @@
-
-require('./bootstrap');
-window.Vue = require('vue');
-require('./Lumino/chart.min');
-require('./Lumino/chart-data');
-require('./Lumino/easypiechart');
-require('./Lumino/easypiechart-data');
-require('./Lumino/bootstrap-datepicker');
-require('./Lumino/dataTables.responsive.min')
-require('./Lumino/dataTables.fixedColumns.min')
+require('./bootstrap')
+window.Vue = require('vue')
+require('datatables.net-bs')
+require('datatables.net-responsive-bs')
 require('./Lumino/jquery.dataTables.min')
 require('./Lumino/jquery.dataTables.bootstrap.min')
-require('./Lumino/select2')
-require('./Lumino/custom');
-require('./custom');
+require('select2')
+require('./Lumino/custom')
+require('./custom')
