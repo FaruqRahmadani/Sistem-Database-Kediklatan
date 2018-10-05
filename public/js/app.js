1fc25facb9cbec07a78e1896f9a41b8c9faebf70
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-10-05 09:34:19 +0800

    styling datatable responsive

--- public/js/app.js
+++ public/js/app.js
@@ -85954,9 +85954,51 @@ $(document).ready(function () {
 
 /***/ }),
 /* 64 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-throw new Error("Module build failed: SyntaxError: Unexpected token (29:0)\n\n\u001b[0m \u001b[90m 27 | \u001b[39m\n \u001b[90m 28 | \u001b[39m\u001b[36mexport\u001b[39m \u001b[36mdefault\u001b[39m {\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 29 | \u001b[39m\u001b[33m<<\u001b[39m\u001b[33m<<\u001b[39m\u001b[33m<<\u001b[39m\u001b[33m<\u001b[39m \u001b[33mHEAD\u001b[39m\n \u001b[90m    | \u001b[39m\u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 30 | \u001b[39m  props\u001b[33m:\u001b[39m [\u001b[32m'api'\u001b[39m\u001b[33m,\u001b[39m \u001b[32m'provinsi'\u001b[39m\u001b[33m,\u001b[39m \u001b[32m'kota'\u001b[39m\u001b[33m,\u001b[39m \u001b[32m'disabled'\u001b[39m\u001b[33m,\u001b[39m \u001b[32m'required'\u001b[39m]\u001b[33m,\u001b[39m\n \u001b[90m 31 | \u001b[39m\u001b[33m===\u001b[39m\u001b[33m===\u001b[39m\u001b[33m=\u001b[39m\n \u001b[90m 32 | \u001b[39m  props\u001b[33m:\u001b[39m [\u001b[32m'api'\u001b[39m\u001b[33m,\u001b[39m \u001b[32m'kota'\u001b[39m\u001b[33m,\u001b[39m \u001b[32m'disabled'\u001b[39m]\u001b[33m,\u001b[39m\u001b[0m\n");
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  props: ['api', 'kota', 'disabled', 'required'],
+  data: function data() {
+    return {
+      datakota: '',
+      KotaId: this.kota,
+      disable: this.disabled
+    };
+  },
+  mounted: function mounted() {
+    this.showKota();
+  },
+  methods: {
+    showKota: function showKota() {
+      var _this = this;
+
+      axios({
+        method: 'get',
+        url: '/api/datakota/',
+        headers: { Authorization: 'Bearer ' + this.api }
+      }).then(function (response) {
+        _this.datakota = response.data;
+      });
+    }
+  }
+});
 
 /***/ }),
 /* 65 */
@@ -85968,66 +86010,6 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c("div", [
     _c("div", { staticClass: "form-group" }, [
-      _vm._v("\n<<<<<<< HEAD\n      "),
-      _c("label", { staticClass: "col-md-2 control-label" }, [
-        _vm._v("Provinsi")
-      ]),
-      _vm._v(" "),
-      _c("div", { staticClass: "col-md-10" }, [
-        _c(
-          "select",
-          {
-            directives: [
-              {
-                name: "model",
-                rawName: "v-model",
-                value: _vm.ProvinsiId,
-                expression: "ProvinsiId"
-              }
-            ],
-            staticClass: "form-control input-lg",
-            attrs: {
-              name: "provinsi_id",
-              disabled: _vm.disable == 1,
-              required: !_vm.required
-            },
-            on: {
-              change: [
-                function($event) {
-                  var $$selectedVal = Array.prototype.filter
-                    .call($event.target.options, function(o) {
-                      return o.selected
-                    })
-                    .map(function(o) {
-                      var val = "_value" in o ? o._value : o.value
-                      return val
-                    })
-                  _vm.ProvinsiId = $event.target.multiple
-                    ? $$selectedVal
-                    : $$selectedVal[0]
-                },
-                _vm.showKota
-              ]
-            }
-          },
-          [
-            _vm._v("=>Provinsi"),
-            _vm._v(" "),
-            _vm._l(this.dataprovinsi, function(dataprovinsi) {
-              return _c("option", { domProps: { value: dataprovinsi.id } }, [
-                _vm._v(_vm._s(dataprovinsi.nama))
-              ])
-            })
-          ],
-          2
-        )
-      ])
-    ]),
-    _vm._v(" "),
-    _c("div", { staticClass: "form-group" }, [
-      _vm._v(
-        "\n=======\n>>>>>>> 56f847f988bda82c37cc4c4b9f7dd8ed830b7035\n      "
-      ),
       _c("label", { staticClass: "col-md-2 control-label" }, [
         _vm._v("Kab/Kota")
       ]),
