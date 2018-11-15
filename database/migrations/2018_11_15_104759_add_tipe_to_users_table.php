3398aa3f3136dcec1bd2823b9408df33798af56c
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-11-15 18:56:10 +0800

    fix typo tipe data migration

--- database/migrations/2018_11_15_104759_add_tipe_to_users_table.php
+++ database/migrations/2018_11_15_104759_add_tipe_to_users_table.php
@@ -14,7 +14,7 @@ class AddTipeToUsersTable extends Migration
   public function up()
   {
     Schema::table('users', function (Blueprint $table) {
-      $table->tinyInt('tipe')->default(5)->after('foto');
+      $table->tinyInteger('tipe')->default(5)->after('foto');
     });
   }
 
