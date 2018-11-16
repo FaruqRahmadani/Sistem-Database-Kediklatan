3351080ba04fda28e0a9f822cacbd7ecc94f3209
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-11-16 21:00:59 +0800

    tampilan form edit data p4s

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Kota;
+use App\P4S;
+use Auth;
 
 class HomeController extends Controller
 {
-  public function index()
-  {
-    return view('home');
+  public function ubahData(){
+    $auth = Auth::User();
+    if ($auth->tipe == 3) return $this->ubahDataP4SForm($auth);
   }
 
-  public function Blank(){
-    return view('User.Blank');
+  public function ubahDataP4SForm($auth){
+    $P4S = P4S::findOrFail($auth->Data->id);
+    $Kota = Kota::all();
+    return view('PesertaAuth.ubahDataP4S', compact('P4S', 'Kota'));
   }
 }
---
+++ resources/views/PesertaAuth/ubahDataP4S.blade.php
@@ -0,0 +1,83 @@
+@extends('Layouts.Master')
+@section('content')
+	<div class="row row-header">
+		<div class="col-lg-12">
+			<h3 class="page-header">Ubah Data
+        <small>P4S</small>
+      </h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<a href="{{Route('p4sData')}}" class="btn btn-primary btn-sm">
+						<span class="fa fa-reply img-circle text-default"></span>
+						Kembali
+					</a>
+				</div>
+				<div class="panel-body">
+					<form class="form-horizontal row-border" action="{{Route('p4sEditSubmit', ['id' => $P4S->UUID])}}" method="POST" enctype="multipart/form-data">
+						@csrf
+						<div class="form-group">
+							<label class="col-md-2 control-label">NIK/NIP</label>
+							<div class="col-md-10">
+								<input type="text" name="nip" class="form-control" value="{{$P4S->nip}}" required>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-2 control-label">Nama</label>
+							<div class="col-md-10">
+								<input type="text" name="nama" class="form-control" required value="{{$P4S->nama}}">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-2 control-label">Nama Ketua</label>
+							<div class="col-md-10">
+								<input type="text" name="nama_ketua" class="form-control" value="{{$P4S->nama_ketua}}">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-2 control-label">Foto Ketua Kelompok</label>
+							<div class="col-md-10">
+								<input type="file" name="foto" class="form-control">
+								<small>*Ukuran Foto 1:1 *Isi hanya jika ganti foto</small>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-2 control-label">Nomor HP</label>
+							<div class="col-md-10">
+								<input type="text" name="nomor_hp" class="form-control" value="{{$P4S->nomor_hp}}">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-2 control-label">Alamat</label>
+							<div class="col-md-10">
+								<textarea name="alamat" class="form-control" rows="4">{{$P4S->alamat}}</textarea>
+							</div>
+						</div>
+						<div class="form-group">
+				      <label class="col-md-2 control-label">Kota</label>
+				      <div class="col-md-10">
+				        <select name="kota_id" class="form-control input-lg">
+									<option value="">Kota</option>
+									@foreach ($Kota as $DataKota)
+										<option value="{{$DataKota->id}}" {{$DataKota->id==$P4S->kota_id? 'selected':''}}>{{$DataKota->nama}}</option>
+									@endforeach
+				        </select>
+				      </div>
+				    </div>
+						<div class="row">
+							<div class="text-center">
+								<div class="col-md-12">
+									<button id="submit" type="submit" class="btn btn-info btn-fill">Simpan</button>
+									<button type="reset" class="btn btn-warning btn-fill">Batal</button>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
