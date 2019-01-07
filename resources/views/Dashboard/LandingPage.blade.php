9e2c333d0299279b3ba996fee16f585eadd36a31
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-01-07 23:50:06 +0800

    setup awal landing page baru

--- resources/views/Dashboard/LandingPage.blade.php
+++ resources/views/Dashboard/LandingPage.blade.php
@@ -1,56 +1,129 @@
-@extends('Layouts.Master')
-@section('content')
-<div class="row">
-	<div class="col-md-12">
-		<div class="panel panel-default">
-			{{-- <div class="panel-heading">Informasi Aplikasi</div> --}}
-			<div class="panel-body">
-				<blockquote><h2 style="margin:0;">Selamat datang di Sistem Database Kediklatan</h2>BBPP Binuang - Kalimantan Selatan</blockquote>
+<!DOCTYPE html>
+<html lang="en">
 
-				<p>Sistem Database Kediklatan dapat membantu dalam pengelolaan data kediklatan seperti data pelatihan, data penyuluh, data komoditas, dan data peserta pelatihan.</p>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>BBPP Binuang</title>
+  <link href="{{asset('css/landingpage/main.css')}}" rel="stylesheet">
+</head>
 
-			</div>
-		</div>
-	</div>
-</div>
-<div class="panel panel-container">
-	<div class="row">
-	<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
-		<div class="panel panel-teal panel-widget border-right">
-			<div class="row no-padding">
-				<em class="fa fa-xl fa-user color-blue"></em>
-				<div class="large">{{$Penyuluh->count()}}</div>
-				<div class="text-muted">Penyuluh</div>
-			</div>
-		</div>
-	</div>
-	<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
-		<div class="panel panel-blue panel-widget border-right">
-			<div class="row no-padding">
-				<em class="fa fa-xl fa-users color-orange"></em>
-				<div class="large">{{$KelompokTani->count()}}</div>
-				<div class="text-muted">Kelompok Tani</div>
-			</div>
-		</div>
-	</div>
-	<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
-		<div class="panel panel-orange panel-widget border-right">
-			<div class="row no-padding">
-				<em class="fa fa-xl fa-archive color-teal"></em>
-				<div class="large">{{$Komoditas->count()}}</div>
-				<div class="text-muted">Komoditas</div>
-			</div>
-		</div>
-	</div>
-	<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
-		<div class="panel panel-red panel-widget ">
-			<div class="row no-padding">
-				<em class="fa fa-xl fa-book color-red"></em>
-				<div class="large">{{$Pelatihan->count()}}</div>
-				<div class="text-muted">Pelatihan</div>
-			</div>
-		</div>
-	</div>
-</div>
-</div>
-@endsection
+<body id="top">
+  <header>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">
+            <img src="{{asset('img/logofix.png')}}" alt="Logo">
+          </a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a class="scrollLink" href="#top">BERANDA</a></li>
+            <li><a class="scrollLink" href="#info">INFORMASI</a></li>
+            <li><a class="scrollLink" href="#contact">KONTAK</a></li>
+            <li><a class="scrollLink" href="#">LOGIN</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <div id="hero">
+    <div class="container">
+      <div class="row mb">
+          <div class="col-md-12">
+            <img class="img-fluid img-responsive" src="img/slides/office.jpg" alt="image">
+          </div>
+      </div>
+    </div>
+  </div>
+  <div id="about">
+    <div class="container">
+      <div class="row text-center">
+        <h1 class="title">SELAMAT DATANG</h1>
+        <p>Sistem Database Kediklatan digunakan untuk mempermudah dalam pengelolaan data penyuluh, kelompok tani, data komoditas, dan data pelatihan.</p>
+        <p>Untuk memulai aplikasi ini, silahkah pilih <strong>Login</strong> pada menu navigasi. Pastikan anda mempunyai username dan passwork untuk masuk ke Aplikasi</p>
+      </div>
+    </div>
+  </div>
+  <div id="info">
+    <div class="container">
+      <h1 class="title text-center">INFORMASI</h1>
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="hero-widget">
+            <div class="icon">
+              <i class="glyphicon glyphicon-user"></i>
+            </div>
+            <div class="text">
+              <var>100</var>
+            </div>
+            <div class="info-title">DATA PENYULUH</div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="hero-widget">
+            <div class="icon">
+              <i class="glyphicon glyphicon-grain"></i>
+            </div>
+            <div class="text">
+              <var>10</var>
+            </div>
+            <div class="info-title">DATA KELOMPOK TANI</div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="hero-widget">
+            <div class="icon">
+              <i class="glyphicon glyphicon-leaf"></i>
+            </div>
+            <div class="text">
+              <var>5</var>
+            </div>
+            <div class="info-title">DATA KOMODITAS</div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="hero-widget">
+            <div class="icon">
+              <i class="glyphicon glyphicon-certificate"></i>
+            </div>
+            <div class="text">
+              <var>12</var>
+            </div>
+            <div class="info-title">PELATIHAN</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="contact">
+    <div class="container">
+      <div class="row text-center">
+          <h1 class="title">KONTAK</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, quo. <br>
+            <strong>Phone :</strong> 0812 3456 7890 <br>
+            <strong>Fax :</strong> 0812 3456 1234 <br>
+            <strong>Email :</strong> email@email.com
+          </p>
+        </div>
+    </div>
+  </div>
+  <footer>
+    <div class="container">
+      <div class="text-center">Balai Besar Pelatihan Pertanian - 2018</div>
+    </div>
+  </footer>
+	<script src="{{asset('js/landingpage/jquery.js')}}"></script>
+	<script src="{{asset('js/landingpage/main.js')}}"></script>
+	<script src="{{asset('js/landingpage/bootstrap.min.js')}}"></script>
+</body>
+
+</html>
